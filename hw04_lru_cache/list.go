52a83aff127567c521e8e56957b6d152e065f295
dmnbars <dmnbars@gmail.com>
2022-12-01 18:00:31 +0100

    first realisation of list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,104 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	list  map[*ListItem]struct{}
+	front *ListItem
+	back  *ListItem
+}
+
+func (l *list) Len() int {
+	return len(l.list)
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	current := &ListItem{Value: v}
+	l.list[current] = struct{}{}
+	if l.front != nil {
+		current.Next = l.front
+		current.Next.Prev = current
+		l.front = current
+
+		return current
+	}
+
+	l.front = current
+	l.back = current
+
+	return current
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	current := &ListItem{Value: v}
+	l.list[current] = struct{}{}
+
+	if l.back != nil {
+		current.Prev = l.back
+		current.Prev.Next = current
+		l.back = current
+
+		return current
+	}
+
+	l.front = current
+	l.back = current
+
+	return current
+}
+
+func (l *list) Remove(i *ListItem) {
+	if _, ok := l.list[i]; !ok {
+		return
+	}
+	delete(l.list, i)
+
+	count := l.Len()
+	if count == 0 {
+		l.front = nil
+		l.back = nil
+
+		return
+	}
+
+	if i.Prev != nil && i.Next != nil {
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+
+		return
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = nil
+		l.back = i.Prev
+		if count == 1 {
+			l.front = l.back
+		}
+	}
+	if i.Next != nil {
+		i.Next.Prev = nil
+		l.front = i.Next
+		if count == 1 {
+			l.back = l.front
+		}
+	}
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	l.PushFront(i.Value)
 }
 
 func NewList() List {
-	return new(list)
+	return &list{
+		list:  map[*ListItem]struct{}{},
+		front: nil,
+		back:  nil,
+	}
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -15,6 +15,76 @@ func TestList(t *testing.T) {
 		require.Nil(t, l.Back())
 	})
 
+	t.Run("test push front", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(10)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+	})
+
+	t.Run("test push back", func(t *testing.T) {
+		l := NewList()
+		l.PushBack(20)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 20, l.Front().Value)
+		require.Equal(t, 20, l.Back().Value)
+	})
+
+	t.Run("test remove last item", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(10)
+
+		item := l.Front()
+		l.Remove(item)
+
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+	})
+
+	t.Run("test remove front item", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(10)
+		l.PushBack(20)
+
+		item := l.Front()
+		l.Remove(item)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 20, l.Front().Value)
+		require.Equal(t, 20, l.Back().Value)
+	})
+
+	t.Run("test remove back item", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(10)
+		l.PushBack(20)
+
+		item := l.Back()
+		l.Remove(item)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+	})
+
+	t.Run("test remove middle item", func(t *testing.T) {
+		l := NewList()
+		l.PushFront(10)
+		l.PushBack(20)
+		item := l.Back()
+		l.PushBack(30)
+
+		l.Remove(item)
+
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 30, l.Back().Value)
+	})
+
 	t.Run("complex", func(t *testing.T) {
 		l := NewList()
 
